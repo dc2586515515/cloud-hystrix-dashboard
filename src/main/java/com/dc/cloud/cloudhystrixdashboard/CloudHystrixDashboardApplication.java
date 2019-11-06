b25c28fe40b00cc555041bf6ded7519fb4780b0b
dc <2586515115@qq.com>
2019-11-06 08:42:41 +0800

    SpringCloud   Hystrix Dashboard

---
+++ src/main/java/com/dc/cloud/cloudhystrixdashboard/CloudHystrixDashboardApplication.java
@@ -0,0 +1,15 @@
+package com.dc.cloud.cloudhystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class CloudHystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudHystrixDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dc/cloud/cloudhystrixdashboard/CloudHystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.cloud.cloudhystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudHystrixDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
